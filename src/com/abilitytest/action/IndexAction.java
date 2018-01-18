d89306ef0aa5957b691d3ed391661b51936cde8e
kk <350968873@qq.com>
2018-01-18 19:39:25 +0800

    修复interfacetest测试文件夹消失问题

--- src/com/abilitytest/action/IndexAction.java
+++ src/com/abilitytest/action/IndexAction.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class IndexAction {
-
-	@RequestMapping(value="welcome")
-	public String welcome(){
-		return "interfacetest/welcome"; 
-	}
 	
 	@RequestMapping(value="test")
 	public String test(){
