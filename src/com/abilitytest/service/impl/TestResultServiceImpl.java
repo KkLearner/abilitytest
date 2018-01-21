6a224cac17d80e7c79165d3e34469bb155cd2930
kk <350968873@qq.com>
2018-01-21 11:26:21 +0800

    修复获取某次测试结果接口显示问题

--- src/com/abilitytest/service/impl/TestResultServiceImpl.java
+++ src/com/abilitytest/service/impl/TestResultServiceImpl.java
@@ -18,7 +18,7 @@ public class TestResultServiceImpl extends BaseServiceImpl<TestResult> implement
 	
 	@Override
 	public List<Map<String, Object>> getAllResults(Integer testid){
-		String sql ="SELECT a.testNumber,b.`name`,b.id_number,a.answer,a.usetime,date_format(a.finishtime,'%Y-%m-%d %T')"
+		String sql ="SELECT a.testNumber,b.`name`,b.id_number,a.answer,a.usetime,date_format(a.finishtime,'%Y-%m-%d %T') as finishtime"
 				+" from testresult as a,disabledman as b,testpool as c"
 				+" WHERE c.id="+testid+" and b.id=c.person_id and a.testpool_id=c.id"
 				+" and a.person_id=b.id and a.if_del=0 and b.if_del=0 and c.if_del=0"
