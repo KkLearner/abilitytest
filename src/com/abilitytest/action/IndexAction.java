24168b9a2186a327ab00fbf24e0cbac3e4761d6b
MikeFengSCNU <895532617mike@gmail.com>
2018-02-05 22:17:28 +0800

    添加了测试二到测试九和题目列表的跳转逻辑

--- src/com/abilitytest/action/IndexAction.java
+++ src/com/abilitytest/action/IndexAction.java
@@ -38,6 +38,51 @@ public class IndexAction {
 		return "test1"; 
 	}
 	
+	@RequestMapping(value="test2")
+	public String test2(){
+		return "test2"; 
+	}
+	
+	@RequestMapping(value="test3")
+	public String test3(){
+		return "test3"; 
+	}
+	
+	@RequestMapping(value="test4")
+	public String test4(){
+		return "test4"; 
+	}
+	
+	@RequestMapping(value="test5")
+	public String test5(){
+		return "test5"; 
+	}
+	
+	@RequestMapping(value="test6")
+	public String test6(){
+		return "test6"; 
+	}
+	
+	@RequestMapping(value="test7")
+	public String test7(){
+		return "test7"; 
+	}
+	
+	@RequestMapping(value="test8")
+	public String test8(){
+		return "test8"; 
+	}
+	
+	@RequestMapping(value="test9")
+	public String test9(){
+		return "test9"; 
+	}
+	
+	@RequestMapping(value="list")
+	public String list(){
+		return "list"; 
+	}
+	
 	@RequestMapping(value="interfacetest/{path}")
 	public String load(@PathVariable String path){
 		return "interfacetest/"+path; 
